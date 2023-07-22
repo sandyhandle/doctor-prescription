590396d0b6c6c2a97aa80f68e6f05f2577dbd4f7
Santhosh_Puvaneswaran <santhosh.puvaneswaran@gmail.com>
2023-07-22 15:14:13 +0530

    gitignore is added and project is initialized.. :)

---
+++ doctor-prescription/src/main/java/com/sandyhandle/webapp/doctorprescription/DoctorPrescriptionApplication.java
@@ -0,0 +1,13 @@
+package com.sandyhandle.webapp.doctorprescription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorPrescriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorPrescriptionApplication.class, args);
+	}
+
+}
