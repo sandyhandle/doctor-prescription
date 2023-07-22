0660afffc51d49fa789f3eb9c5a8709a7ece623d
Santhosh Puvaneswaran <64911148+sandyhandle@users.noreply.github.com>
2023-07-22 15:15:31 +0530

    gitignore is added and project is initialized.. :) (#2)

---
+++ doctor-prescription/src/test/java/com/sandyhandle/webapp/doctorprescription/DoctorPrescriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandyhandle.webapp.doctorprescription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorPrescriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
